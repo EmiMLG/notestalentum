5d3d444439046bd9f93bb0de2b4837b0f123bd54
Emi <emimlg1@gmail.com>
2017-01-19 13:43:14 +0100

    add viewrecycler

--- app/src/main/java/es/telefonica/talentum/noteapp/NoteListFragment.java
+++ app/src/main/java/es/telefonica/talentum/noteapp/NoteListFragment.java
@@ -3,6 +3,8 @@ package es.telefonica.talentum.noteapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class NoteListFragment extends Fragment {
 
+    private RecyclerView recyclerNotesView;
 
     public NoteListFragment() {
         // Required empty public constructor
@@ -23,7 +26,16 @@ public class NoteListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+
+        recyclerNotesView = (RecyclerView) view.findViewById(R.id.fragment_note_list_note_list);
+        recyclerNotesView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
     }
 
+    public void setAdapter(NoteAdapter adapter) {
+        recyclerNotesView.setAdapter(adapter);
+    }
 }
